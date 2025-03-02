66d4f0055b07948b9bf0a6628d0b822cfccabd31
Jack Winkler <jack.p.winkler@gmail.com>
2025-03-02 06:59:59 -0500

    Update icons

--- next_app/app/(root)/page.tsx
+++ next_app/app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import blood_logo from "../../public/blood_logo.png";
+import blood_logo from "../../public/logo_NEW_.png";
 import Link from "next/link";
 import Bell_headshot from "../../public/Bell_headshot.jpeg";
 import Julian_headshot from "../../public/Julian_headshot.jpg";
@@ -20,17 +20,18 @@ export default function Home() {
         </div>
         <div id="subsection">
           <nav className="flex gap-6">
-            <a href="#main-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600 transition-transform transform group-hover:scale-110 group-hover:shadow-xl">Home</a>
-            <a href="#about-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600">About</a>
-            <a href="#about-us-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600">About Us</a>
+            <a href="#main-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600 transition-transform transform hover:scale-110 hover:shadow-xl">Home</a>
+            <a href="#about-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600 transition-transform transform hover:scale-110 hover:shadow-xl">Overview</a>
+            <a href="#about-us-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600 transition-transform transform hover:scale-110 hover:shadow-xl">About Us</a>
+            <a href="#future-plans-section" className="px-5 py-2 rounded-full bg-gray-700 text-white font-bold shadow-md hover:bg-gray-600 transition-transform transform hover:scale-110 hover:shadow-xl">Future Plans</a>
           </nav>
         </div>
         <div id="login">
           <div className="flex gap-4">
-            <Link href="/signup" className="px-6 py-3 rounded-full bg-green-600 text-white font-bold shadow-lg hover:bg-green-500">
+            <Link href="/signup" className="px-6 py-3 rounded-full bg-green-600 text-white font-bold shadow-lg hover:bg-green-500 transition-transform transform hover:scale-110 hover:shadow-xl">
               Sign Up
             </Link>
-            <Link href="/login" className="px-6 py-3 rounded-full bg-gray-700 text-white font-bold shadow-lg hover:bg-gray-600">
+            <Link href="/login" className="px-6 py-3 rounded-full bg-gray-700 text-white font-bold shadow-lg hover:bg-gray-600 transition-transform transform hover:scale-110 hover:shadow-xl">
               Login
             </Link>
           </div>
@@ -40,14 +41,14 @@ export default function Home() {
       <div id="main-section" className="relative text-center mt-14 z-10">
         <h1 className="text-6xl font-bold mb-3">Welcome to BloodQwuirk!<br/></h1>
         <h3 className="text-2xl font-bold">Understanding your blood work, made simple</h3>
-        <div className="bg-gray-400 p-1 max-w-3xl mx-auto rounded-xl shadow-md mt-14 z-8">
-          <p className="text-lg">Your AI-powered blood analysis tool. Login and upload your blood test results to generate a report and summary.</p>
-          <p className="text-lg">Our cutting-edge AI technology provides insights into your health by analyzing your blood test results.<br/>Our goal is to make blood test data more accessible and understandable for everyone.</p>
-          <p className="text-lg">Learn more about your health with BloodLens!</p>
+        <div className="bg-gray-400 p-1 max-w-3xl mx-auto rounded-xl shadow-md mt-20 z-8">
+          <p className="text-lg">Your AI-powered blood analysis tool. Easily upload your blood test results and receive a detailed report with clear, AI-driven insights.</p>
+          <p className="text-lg">Our advanced AI technology breaks down complex medical data, helping you understand key health indicators and what they mean for your well-being. Whether you're tracking changes over time or simply curious about your results, we make bloodwork more accessible and easier to interpret.</p>
+          <p className="text-lg">Take control of your health with BloodQwuirk—because understanding your body shouldn't be complicated!</p>
         </div>
       </div>
 
-      <div id="about-us-section" className="relative mt-24 z-10">
+      <div id="about-us-section" className="relative mt-27 z-10">
         <h1 className="text-center text-3xl font-bold">About Us</h1>
         <div className="flex justify-center gap-8 mt-8">
           {[{img: Bell_headshot, name: "Bella Goltser", role: "BACKEND DEVELOPER"},
@@ -64,6 +65,15 @@ export default function Home() {
               </div>
           ))}
         </div>
+
+        <div id="future-plans-section" className="relative text-center mt-27 z-10">
+        <h3 className="text-2xl font-bold">Future Plans</h3>
+        <div className="bg-gray-400 p-1 max-w-3xl mx-auto rounded-xl shadow-md mt-14 z-8">
+          <p className="text-lg">The Future</p>
+          <p className="text-lg">is bright</p>
+          <p className="text-lg">with BloodQwuirk</p>
+        </div>
+      </div>
 
         <div className="flex justify-center mt-16">
           <img 
