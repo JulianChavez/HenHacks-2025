b2a87501a19537adc02ceac0e4e06d059f7c16f6
bgoltser <igoltse1@umbc.edu>
2025-03-01 20:43:22 -0500

    bella update 2

--- next_app/app/(root)/page.tsx
+++ next_app/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import blood_logo from "../../public/blood_logo.png";
 import Link from "next/link";
-import Bell_headshot from "../../public/Bell_headshot.jpg";
+import Bell_headshot from "../../public/Bell_headshot.jpeg";
 import Julian_headshot from "../../public/Julian_headshot.jpg";
 import Jack_headshot from "../../public/Jack_headshot.jpg";
 
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-black before:content-[''] before:absolute before:inset-0 before:bg-[url('/Friday.jpg')] before:bg-cover before:bg-center before:opacity-70">
       </div>
 
-      <div id="header" className="relative rounded-box p-4 bg-opacity-90 bg-gray-900 z-10">
+      <div id="header" className="relative rounded-box p-4 bg-opacity-90 bg-gray-400 z-10">
         <div id="Logo">
           <img src={blood_logo.src} alt="Blood Logo" width={100} height={100} />
         </div>
@@ -30,14 +30,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="main-section" className="relative text-center mt-8 z-10">
-        <h1 className="text-2xl font-bold">Welcome to BloodLens!</h1>
+      <div id="main-section" className="relative text-center mt-14 z-10">
+        <h1 className="text-2xl font-bold">Welcome to BloodLens!<br/><br/></h1>
+        <div className="bg-gray-400 p-1 max-w-lg mx-auto rounded-xl shadow-md mt-14 z-8">
         <p className="text-lg">Your AI-powered blood analysis tool. Login and upload your blood test results to generate a report and summary.</p>
         <p className="text-lg">Our cutting-edge AI technology provides insights into your health by analyzing your blood test results.<br/>Our goal is to make blood test data more accessible and understandable for everyone.</p>
         <p className="text-lg">Learn more about your health with BloodLens!</p>
+        </div>
       </div>
 
-      <div id="about-us-section" className="relative mt-12 z-10">
+      <div id="about-us-section" className="relative mt-24 z-10">
         <h1 className="text-center text-2xl font-bold">About Us</h1>
         <div className="flex justify-center gap-8 mt-8">
           {[{img: Bell_headshot, name: "Bella Goltser", role: "BACKEND DEVELOPER"},
--- next_app/app/login/page.tsx
+++ next_app/app/login/page.tsx
@@ -4,38 +4,41 @@ import Link from "next/link";
 
 export default function Login() {
     return (
-        <div className="login-page-container">
-            <div id="Login-section" className="Login-section">
-                <h1>Login Page</h1>
+        <div className="login-page-container bg-gray-300 min-h-screen flex items-center justify-center">
+            <div id="Login-section" className="Login-section p-8 rounded-lg shadow-lg">
+                <h1 className="font-bold text-xl mb-4">Login Page</h1>
                 
                 {/* Login form would go here */}
-                <div className="w-full max-w-xs mt-4">
+                <div className="w-full max-w-xs">
                     <form className="bg-white rounded px-8 pt-6 pb-8 mb-4">
                         <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            <label className="block text-gray-700 text-sm font-mono mb-2" htmlFor="username">
                                 Username
                             </label>
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
                         </div>
                         <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            <label className="block text-gray-700 text-sm font-mono mb-2" htmlFor="password">
                                 Password
                             </label>
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
                         </div>
                         <div className="flex items-center justify-between">
-                            <Link href="/report_analysis" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign In</Link>
+                            <Link href="/report_analysis" className="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                                Sign In
+                            </Link>
                         </div>
                     </form>
                 </div>
-                
+
                 <div className="mt-4">
                     <Link href="/">
-                        <button className="px-4 py-2 rounded-md bg-gray-500 text-white">Back to Home</button>
+                        <button className="px-4 py-2 rounded-full bg-gray-700 font-bold text-white">
+                            Back to Home
+                        </button>
                     </Link>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
